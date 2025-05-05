b3c2ed44208ffa46b63419e1e4aaaf2654b250f4
AasrithMareddy <aasrith.mareddy@gmail.com>
2025-05-05 16:23:37 +0530

    Initial Commit

---
+++ ShopEasy/ShopEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopEasyApp.swift
+//  ShopEasy
+//
+//  Created by Aasrith Mareddy on 05/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
